f9195e7123d77a3a29196d5a9f2ced41b5a3e239
yourkiwifruit <359641779@qq.com>
2021-04-08 22:35:08 +0800

    * 添加HashMap常用方法含义速记

--- java/util/HashMap.java
+++ java/util/HashMap.java
@@ -36,103 +36,51 @@ import java.util.function.Consumer;
 import java.util.function.Function;
 
 /**
- * Hash table based implementation of the Map interface.  This
- * implementation provides all of the optional map operations, and permits
- * null values and the null key.  (The HashMap
- * class is roughly equivalent to Hashtable, except that it is
- * unsynchronized and permits nulls.)  This class makes no guarantees as to
- * the order of the map; in particular, it does not guarantee that the order
- * will remain constant over time.
+ * 常用方法含义速记：
+ *  void clear(); 删除map中的所有k-v
+ *  Object clone(); 返回HashMap的浅拷贝。k和v本身不会被克隆。
+ *  V compute(K key, BiFunction<? super K,? super V,? extends V> remappingFunction) 拿到指定的key在map中的value，然后放到remappingFunction方法中，不管value存不存在，都进行操作，完成后保存到map中，返回的是新计算的value。
+ *  V computeIfAbsent(K key, Function<? super K,? extends V> mappingFunction) 先查看指定key在map中是否存在，当key不存在时，会执行后面的mappingFunction方法，然后将key和mappingFunction计算的结果存入map中，最终返回新计算的value，否则返回key对应的value。
+ *  V computeIfPresent(K key, BiFunction<? super K,? super V,? extends V> remappingFunction) 先查看指定key在map中是否存在，当key存在时，会执行后面的remappingFunction方法，完成后保存到map中，返回新计算的value，否则返回null。
+ *  boolean containsKey(Object key) 如果map中包含指定的key，就返回true，否则返回false。
+ *  boolean containsValue(Object value) 如果map中包含指定的value，就返回true，否则返回false。
+ *  Set<Map.Entry<K,V>> entrySet() 返回map中包含所有k-v的Set集合。
+ *  void forEach(BiConsumer<? super K,? super V> action) 对此映射中的每个条目执行给定的action操作，直到所有条目都被处理或操作引发异常。
+ *  V get(Object key) 返回到指定key所映射的value或者null，当map中没有指定key存在时，返回null，也有可能指定的key存放的就是null值。
+ *  boolean isEmpty() 如果此地图不包含k-v映射，则返回true。
  *
- * <p>This implementation provides constant-time performance for the basic
- * operations (get and put), assuming the hash function
- * disperses the elements properly among the buckets.  Iteration over
- * collection views requires time proportional to the "capacity" of the
- * HashMap instance (the number of buckets) plus its size (the number
- * of key-value mappings).  Thus, it's very important not to set the initial
- * capacity too high (or the load factor too low) if iteration performance is
- * important.
  *
- * <p>An instance of HashMap has two parameters that affect its
- * performance: initial capacity and load factor.  The
- * capacity is the number of buckets in the hash table, and the initial
- * capacity is simply the capacity at the time the hash table is created.  The
- * load factor is a measure of how full the hash table is allowed to
- * get before its capacity is automatically increased.  When the number of
- * entries in the hash table exceeds the product of the load factor and the
- * current capacity, the hash table is rehashed (that is, internal data
- * structures are rebuilt) so that the hash table has approximately twice the
- * number of buckets.
  *
- * <p>As a general rule, the default load factor (.75) offers a good
- * tradeoff between time and space costs.  Higher values decrease the
- * space overhead but increase the lookup cost (reflected in most of
- * the operations of the HashMap class, including
- * get and put).  The expected number of entries in
- * the map and its load factor should be taken into account when
- * setting its initial capacity, so as to minimize the number of
- * rehash operations.  If the initial capacity is greater than the
- * maximum number of entries divided by the load factor, no rehash
- * operations will ever occur.
  *
- * <p>If many mappings are to be stored in a HashMap
- * instance, creating it with a sufficiently large capacity will allow
- * the mappings to be stored more efficiently than letting it perform
- * automatic rehashing as needed to grow the table.  Note that using
- * many keys with the same {@code hashCode()} is a sure way to slow
- * down performance of any hash table. To ameliorate impact, when keys
- * are {@link Comparable}, this class may use comparison order among
- * keys to help break ties.
  *
- * <p><strong>Note that this implementation is not synchronized.</strong>
- * If multiple threads access a hash map concurrently, and at least one of
- * the threads modifies the map structurally, it must be
- * synchronized externally.  (A structural modification is any operation
- * that adds or deletes one or more mappings; merely changing the value
- * associated with a key that an instance already contains is not a
- * structural modification.)  This is typically accomplished by
- * synchronizing on some object that naturally encapsulates the map.
  *
- * If no such object exists, the map should be "wrapped" using the
- * {@link Collections#synchronizedMap Collections.synchronizedMap}
- * method.  This is best done at creation time, to prevent accidental
- * unsynchronized access to the map:<pre>
- *   Map m = Collections.synchronizedMap(new HashMap(...));</pre>
  *
- * <p>The iterators returned by all of this class's "collection view methods"
- * are fail-fast: if the map is structurally modified at any time after
- * the iterator is created, in any way except through the iterator's own
- * remove method, the iterator will throw a
- * {@link ConcurrentModificationException}.  Thus, in the face of concurrent
- * modification, the iterator fails quickly and cleanly, rather than risking
- * arbitrary, non-deterministic behavior at an undetermined time in the
- * future.
  *
- * <p>Note that the fail-fast behavior of an iterator cannot be guaranteed
- * as it is, generally speaking, impossible to make any hard guarantees in the
- * presence of unsynchronized concurrent modification.  Fail-fast iterators
- * throw ConcurrentModificationException on a best-effort basis.
- * Therefore, it would be wrong to write a program that depended on this
- * exception for its correctness: the fail-fast behavior of iterators
- * should be used only to detect bugs.
  *
- * <p>This class is a member of the
- * <a href="{@docRoot}/../technotes/guides/collections/index.html">
- * Java Collections Framework</a>.
  *
- * @param <K> the type of keys maintained by this map
- * @param <V> the type of mapped values
  *
- * @author  Doug Lea
- * @author  Josh Bloch
- * @author  Arthur van Hoff
- * @author  Neal Gafter
- * @see     Object#hashCode()
- * @see     Collection
- * @see     Map
- * @see     TreeMap
- * @see     Hashtable
- * @since   1.2
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
  */
 public class HashMap<K,V> extends AbstractMap<K,V>
     implements Map<K,V>, Cloneable, Serializable {
